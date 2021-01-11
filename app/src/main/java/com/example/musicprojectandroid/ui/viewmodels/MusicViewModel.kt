29acf33c0a7810d5c61c2818e8884768469f920a
Sophie Vaury <sophie.vaury@ratp.fr>
2021-01-11 23:16:51 +0100

    add live data

--- app/src/main/java/com/example/musicprojectandroid/ui/viewmodels/MusicViewModel.kt
+++ app/src/main/java/com/example/musicprojectandroid/ui/viewmodels/MusicViewModel.kt
@@ -2,12 +2,16 @@ package com.example.musicprojectandroid.ui.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.musicprojectandroid.model.Music
 import com.example.musicprojectandroid.repository.MusicRepository
+import com.example.musicprojectandroid.utils.Data
+import kotlinx.coroutines.Dispatchers
 
 class MusicViewModel : ViewModel() {
      val musicRepository = MusicRepository()
 
-    val musicList: LiveData<List<Music>> =
-        musicRepository.getAllMusicsFromApi().asLiveData(viewModelScope.coroutineContext)
+    val musicList: LiveData<Data<List<Music>>> = musicRepository.getAllMusicsFromApi().asLiveData(Dispatchers.IO + viewModelScope.coroutineContext)
+
 }
